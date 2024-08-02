105ee0ab51ea3b740d89ff58efe98f1b8b0569df
NaufalMayland <maylandzaa13@gmail.com>
2024-08-02 14:11:15 +0700

    update

--- app/Http/Controllers/ControllerLogin.php
+++ app/Http/Controllers/ControllerLogin.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 
@@ -16,27 +15,26 @@ class ControllerLogin extends Controller
 
     public function login(Request $request){
         $credentials = $request->post();
-        // dd($credentials);
-            $user = User::where('email', $credentials['email'])->first();
-            if(is_null($user)){
-                return redirect()->back()->withErrors('Email dan Password tidak sesuai')->withInput();
-            }
+        $user = User::where('email', $credentials['email'])->first();
 
-            if(!Hash::check($credentials['password'], $user->password)){
-                return redirect()->back()->withErrors('Password tidak sesuai')->withInput();
-            }
+        if(is_null($user)){
+            return redirect()->back()->withErrors('Email dan Password tidak sesuai')->withInput();
+        }
+        if(!Hash::check($credentials['password'], $user->password)){
+            return redirect()->back()->withErrors('Password tidak sesuai')->withInput();
+        }
 
-            $request->session()->put('id_users', $user->id_users);
-            if($user->role=='admin'){
-                return redirect('/admin');
-            }elseif($user->role=='guru'){
-                return redirect('/guru');
-            }elseif($user->role=='siswa'){
-                return redirect('/siswa');
-            }
-            else{
-                return redirect()->back()->withErrors('Akun tidak ada!')->withInput();
-            }
+        $request->session()->put('id_users', $user->id_users);
+        
+        if($user->role=='admin'){
+            return redirect('/admin');
+        }elseif($user->role=='guru'){
+            return redirect('/guru');
+        }elseif($user->role=='siswa'){
+            return redirect('/siswa');
+        }else{
+            return redirect()->back()->withErrors('Akun tidak ada!')->withInput();
+        }
     }
 
     public function logout(Request $request){
